7876ae171b60fb09130b3aec645483ea16f9a125
Phillip Shannon & Tyler Deem <student@epicodus.com>
2015-08-05 15:17:54 -0700

    Created a way to enter locations into a stored list and be able to delete them

--- src/Place.php
+++ src/Place.php
@@ -11,12 +11,27 @@ class Place
 
     function setLocation($new_location)
     {
-        $this->location = (string) $new_description;
+        $this->location = (string) $new_location;
     }
 
     function getLocation()
     {
         return $this->location;
+    }
+
+    static function getAll()
+    {
+        return $_SESSION['list_of_places'];
+    }
+
+    function save()
+    {
+        array_push($_SESSION['list_of_places'], $this);
+    }
+
+    static function deleteAll()
+    {
+        $_SESSION['list_of_places'] = array();
     }
 }
 ?>
